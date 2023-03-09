1e6210d0a286aacff9b8b4510c0a8d173657d362
Aluno UniAcademia <alunouni@labcesac.net>
2023-03-09 19:12:16 -0300

    adição bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './bootstrap/css/bootstrap.min.css'
 import './App.css';
 import Header from './header';
 import Aside from './aside';
@@ -5,11 +6,28 @@ import Footer from './footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Aside />
-      <Footer />
-    </div>
+    <html>
+      <head>
+        <title>Projeto Dia a Dia</title>
+      </head>
+      <body class="App">
+        <div class="row">
+          <div class="col-lg-12 header">
+            <Header />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 aside">
+            <Aside />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 footer">
+            <Footer />
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
 
